3420b3fcef6d318d0a161275cdd056cb91b7ee99
sahil dhanawade <sahildhanavade769@gmail.com>
2022-09-21 19:42:16 +0530

    final changes done

--- components/Poster.jsx
+++ components/Poster.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import FileDownloadDoneIcon from '@mui/icons-material/FileDownloadDone';
+import Image from 'next/image'
 
 const Poster = (props) => {
     return <>
         <div className="poster paddingrl">
+
+            <div className="humansimg" >
+            <Image src="/images/phoneperson.png" width="400" height="303" />
+            </div>
+
             <h1 className='tagline'>Savings to Set Your Website in Motion</h1>
             <h1 className="price"> 149.00</h1>
             <p className="para">Get things moving with a free domain, lifetime SSL, and DDos protection included with a four-year subscription</p>
